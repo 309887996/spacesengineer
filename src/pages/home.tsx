3471d855e3eb52cbc23d48d37307bac5a4ff4ba2
huangqijina <13755418232@163.com>
2022-03-01 23:58:36 +0800

    feat: 2

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,7 @@ import './home.less';
 export default function HomePage() {
   return (
     <div className="a">
-      1111<span className="iconfont icon-anticon-shengchanjihua"></span>
+      1111<span className="iconfont icon-anticon-shengchanjihua">2</span>
     </div>
   );
 }
